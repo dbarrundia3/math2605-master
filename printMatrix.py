d7167d06038ec9e7855d719d20c44dae23d63508
dbarrundia3 <dbarrundia3@gatech.edu>
2015-11-05 14:00:00 -0500

    Added the first python file to print matrix.

---
+++ printMatrix.py
@@ -0,0 +1,10 @@
+  def pprint(A):
+    n = len(A)
+    for i in range(0, n):
+        line = ""
+        for j in range(0, n+1):
+            line += str(A[i][j]) + "\t"
+            if j == n-1:
+                line += "| "
+        print(line)
+    print("")
